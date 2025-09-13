53642e7fdeaa853d907e3d79ce34c0471eea2f00
Sierss <kelesh_nikita@mail.ru>
2025-09-13 19:37:20 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-// svelte.config.js
 import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter({ runtime: 'nodejs20.x' })
+  },
   vitePlugin: { inspector: false }
 };
-
 export default config;
+
